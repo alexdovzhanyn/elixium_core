d951d8ed9eedc3487e89db6a887361a31af5e490
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-01-25 20:08:07 -0500

    initial commit

---
+++ lib/ultradark.ex
@@ -0,0 +1,18 @@
+defmodule Ultradark do
+  @moduledoc """
+  Documentation for Ultradark.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ultradark.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ultradark_test.exs
@@ -0,0 +1,8 @@
+defmodule UltradarkTest do
+  use ExUnit.Case
+  doctest Ultradark
+
+  test "greets the world" do
+    assert Ultradark.hello() == :world
+  end
+end
