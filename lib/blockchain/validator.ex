38c537fc10fd04623d0d9c7699784fa6b36dce6c
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-02-01 21:46:40 -0500

    refactor validator

--- lib/blockchain/validator.ex
+++ lib/blockchain/validator.ex
@@ -13,41 +13,51 @@ defmodule UltraDark.Validator do
 
     with :ok <- valid_index(block.index, last_block.index),
        :ok <- valid_prev_hash(block.previous_hash, last_block.hash),
-       :ok <- valid_hash(block)
+       :ok <- valid_hash(block),
+       :ok <- valid_coinbase?(block)
     do
       :ok
     else
-      err -> :error
+      :invalid_index -> IO.puts "Block has invalid index"
+      :invalid_prev_hash -> IO.puts "Block previous_hash is not same as the previous block's hash"
+      :invalid_hash -> IO.puts "Block header digest does not match hash"
+      :no_coinbase -> IO.puts "Block does not cointain a coinbase"
+      :invalid_coinbase_type -> IO.puts "Block coinbase txtype is not COINBASE"
+      :invalid_coinbase_output -> IO.puts "Block coinbase  output amount is invalid"
     end
   end
 
   defp valid_index(index, prev_index) when index > prev_index, do: :ok
+  defp valid_index(index, prev_index) when index <= prev_index, do: :invalid_index
+
   defp valid_prev_hash(prev_hash, last_block_hash) when prev_hash == last_block_hash, do: :ok
+  defp valid_prev_hash(prev_hash, last_block_hash) when prev_hash != last_block_hash, do: :invalid_prev_hash
 
   defp valid_hash(%{index: index, previous_hash: previous_hash, timestamp: timestamp, nonce: nonce, hash: hash}) do
-    if Utilities.sha_base16([Integer.to_string(index), previous_hash, timestamp, Integer.to_string(nonce)]) == hash, do: :ok
+    if Utilities.sha_base16([Integer.to_string(index), previous_hash, timestamp, Integer.to_string(nonce)]) == hash, do: :ok, else: :invalid_hash
   end
 
   def valid_coinbase?(%{transactions: transactions, index: block_index}) do
     coinbase = List.first(transactions)
 
-    with :ok <- is_coinbase?(coinbase),
+    with :ok <- (&(if &1 != nil, do: :ok, else: :no_coinbase)).(coinbase),
+       :ok <- is_coinbase?(coinbase),
        :ok <- appropriate_coinbase_output?(transactions, block_index)
     do
       :ok
     else
-      err -> :error
+      err -> err
     end
   end
 
   defp is_coinbase?(tx) do
-    if tx.txtype == "COINBASE", do: :ok
+    if tx.txtype == "COINBASE", do: :ok, else: :invalid_coinbase_type
   end
 
   defp appropriate_coinbase_output?([coinbase | transactions], block_index) do
     total_block_fees = transactions |> Enum.reduce(0, fn tx, acc -> acc + Transaction.calculate_fee(tx) end)
     appropriate_block_reward = Block.calculate_block_reward(block_index)
 
-    if total_block_fees + appropriate_block_reward == List.first(coinbase.outputs).amount, do: :ok
+    if total_block_fees + appropriate_block_reward == List.first(coinbase.outputs).amount, do: :ok, else: :invalid_coinbase_output
   end
 end
