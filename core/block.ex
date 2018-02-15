bb473128ca4cb229748cdc4490e24f538a6d1de1
Zac Garby <zacgarby@icloud.com>
2018-02-15 13:08:50 +0000

    Remove some unnecessary blank lines

--- core/block.ex
+++ core/block.ex
@@ -58,9 +58,7 @@ defmodule UltraDark.Blockchain.Block do
   def mine(block) do
     %{index: index, previous_hash: previous_hash, timestamp: timestamp, nonce: nonce, merkle_root: merkle_root} = block
 
-
     block = %{ block | hash: Utilities.sha3_base16([Integer.to_string(index), previous_hash, timestamp, Integer.to_string(nonce), merkle_root]) }
-
 
     if hash_beat_target?(block) do
       block
