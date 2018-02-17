9a1c565d1906dcf27136af21acf4ed7aaf71a7d1
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-02-17 00:47:06 -0500

    common variables to contract constructor

--- core/contracts/Contract.js
+++ core/contracts/Contract.js
@@ -4,7 +4,11 @@ let UltraDark = {}
 let gamma  = 0
 
 UltraDark.Contract = class  {
-  constructor() {
+  constructor(opts = {}) {
+    this.block_index = opts.block_index
+    this.block_hash = opts.block_hash
+    this.block_nonce = opts.block_nonce
+    this.transaction_id = opts.transaction_id
   }
 
   main() {
