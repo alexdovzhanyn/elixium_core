6c01c069c010d8117b51ee49689a83a94cfc40e8
Zac Garby <me@zacgarby.co.uk>
2018-02-24 12:53:47 +0000

    Add catch-all clauses to to_string and str

--- lib/error.ex
+++ lib/error.ex
@@ -8,6 +8,7 @@ defmodule UltraDark.Error do
   """
   @spec to_string({:error, any}) :: String.t
   def to_string({:error, err}), do: str(err)
+  def to_string(err), do: "error #{err} isn't a valid error tuple"
 
   defp str({:invalid_index, prev, index}), do: "invalid index #{index}, expected #{prev+1}"
   defp str({:wrong_hash, reason}), do: "invalid hash: #{hash_err reason}"
@@ -16,6 +17,7 @@ defmodule UltraDark.Error do
   defp str({:not_coinbase, txtype}), do: "the first transaction is not a coinbase, but a #{txtype}"
   defp str(:invalid_coinbase), do: "the coinbase is invalid, since the fees + reward ≠ coinbase amount"
   defp str({:invalid_difficulty, difficulty, diff}), do: "invalid block difficulty #{difficulty}. expected #{diff}"
+  defp str(err), do: "unrecognized error: #{err}"
 
   defp hash_err({:doesnt_match_last, prev, hash}), do: "doesn't match last hash. expected #{prev}, got #{hash}"
   defp hash_err({:too_low, hash, difficulty}), do: "hash #{hash} is too low for a difficulty of #{difficulty}"
