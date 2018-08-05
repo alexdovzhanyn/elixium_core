d48a4f9af15b929f08c80a3a91bf9d5a4267746f
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-08-04 20:25:19 -0400

    start tcp server

---
+++ lib/p2p/server.ex
@@ -0,0 +1,15 @@
+defmodule Elixium.P2P.Server do
+  use GenServer
+
+  @initial_state %{socket: nil}
+
+  def start_link do
+    GenServer.start_link(__MODULE__, @initial_state)
+  end
+
+  def init(state) do
+    opts = [:binary, active: false]
+    {:ok, socket} = :gen_tcp.connect('localhost', 6379, opts)
+    {:ok, %{state | socket: socket}}
+  end
+end
