43da2feb9b56d585b055869e2b255f74d09c6d72
Alex Dovzhanyn <dovzhanyn.alex@gmail.com>
2018-12-06 13:35:15 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elixium.Mixfile do
   def project do
     [
       app: :elixium_core,
-      version: "0.4.9",
+      version: "0.4.10",
       elixir: "~> 1.7",
       elixirc_paths: ["lib"],
       start_permanent: Mix.env() == :prod,
