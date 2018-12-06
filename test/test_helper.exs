85ea21b65197aa559309c7a4b52e746b4dcabb98
fantypants <8563615@gmail.com>
2018-12-06 02:37:55 -0800

    Test Suite Helper started

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,14 @@
 ExUnit.start(timeout: 600000, capture_log: true)
+
+task = Task.async(fn -> IO.gets("""
+Welcome to the test suite.. Choose what you would like to test:
+0 => Run All Tests
+1 => Block Tests
+2 => Error Tests
+3 => KeyPair Tests
+4 => Node Tests
+5 => Store Tests
+6 => Transaction Tests
+7 => Utilities Tests
+8 => Validator Tests
+""" ) |> String.trim("\n") end)
